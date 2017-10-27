1ba80efac486449c16527b3c63e8f8c63f853f3b
加藤　優奈 <yuvernacrew@gmail.com>
2017-10-28 01:21:17 +0900

    関数化

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php require_once 'twitter.php'; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset='UTF-8'>
+  <meta name='viewport' content='width=device-width,initial-scale=1'>
+  <title>芸工祭2017|TOP</title>
+  <meta name="description" content="名古屋市立芸術工学部の大学祭、芸工祭の公式ホームページです。今年のテーマは「BombJoule」芸工生だけでなく芸工祭に参加してくれるみなさんも内に秘めたエネルギーを思い切り爆発させて新しい自分に出会い、全力で芸工祭を楽しんでほしい！そんな想いが込められています。">
+  <link rel='stylesheet' type='text/css' href='css/ress.min.css'>
+  <link rel='stylesheet' type='text/css' href='css/default.css'>
+
+  <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js'></script>
+</head>
+<body>
+
+</body>
+</html>
